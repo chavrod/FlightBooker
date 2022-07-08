418ee50fbb0ff54cf1dfded270716abe0c8b0289
Dima Chavro <dchavro@gmail.com>
2022-07-08 17:31:30 +0100

    Initialise FlightsController to pull up airports for selection in the #index view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,13 @@ class FlightsController < ApplicationController
   def index
     @origin_options = Airport.all.map { |a| [a.name, a.id] }
     @destination_options = Airport.all.map { |a| [a.name, a.id] }
+
+    # @available_flights = 
+  end
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :takeoff, :number_of_passengers)
   end
 end
