4222f79ad24aba6c585dfb3ae31ec1d8cf080f42
Dima Chavro <dchavro@gmail.com>
2022-07-08 15:36:51 +0100

    Add Flight select options in view and edit controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,6 @@
 class FlightsController < ApplicationController
+  def index
+    @origin_options = Airport.all.map { |a| [a.name, a.id] }
+    @destination_options = Airport.all.map { |a| [a.name, a.id] }
+  end
 end
